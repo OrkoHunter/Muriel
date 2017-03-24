803211bfb523ef4b20fdbd6f0dc54d3d33e8404e
Himanshu Mishra <OrkoHunter@users.noreply.github.com>
2017-03-25 02:32:38 +0530

    Use better method to shuffle

--- play.py
+++ play.py
@@ -10,57 +10,62 @@ if sys.platform in ['win32', 'win64']:
     import itertools
 
 
-'''Load or create the file with statistics, counts.json'''
+'''Load or create the file with mapping, map.json'''
 # Script location
 s_path = os.path.realpath(__file__)
 os.chdir(os.path.sep.join(s_path.split(os.path.sep)[:-1]))
 
-if not os.path.exists("counts.json"):
-    # Read the file list
+if not os.path.exists("map.json"):
 
-    # A dictionary with key as episode and value as number of times watched
+    # Remove files of size less than 10 MB (Subtitles, etc.)
+    for vid in list(files):
+        if os.stat(vid).st_size < 10e7:
+            files.remove(vid)
+
+    # A dictionary with key as episode and value as a unique index
     counts = dict()
 
+    # Read the file list
+    files = None
     if sys.platform in ['win32', 'win64']:
         files = list(itertools.chain.from_iterable([glob('*'), glob('*\\*')]))
     else:
         files = list(glob('*' + os.path.sep + '*'))
 
-    # Initialize the dict with files > 10MB in size
+    random.shuffle(files)
+    # Initialize the dictionary
+    index = 0
     for vid in files:
-        # Remove files of size less than 10 MB (Subtitles, etc.)
-        if os.stat(vid).st_size < 10**7:
-            files.remove(vid)
-        else:
-            # Initialize the dictionary
-            counts[vid] = 0
+        counts[index] = vid
+        index += 1
 
-    # Initialize counts.json
-    with open('counts.json', 'w') as f:
+    # Initialize map.json
+    with open('map.json', 'w') as f:
         f.write(json.dumps(counts))
+    last_watched = -1
+    with open('last_watched.txt', 'w') as f:
+        f.write(str(last_watched))
 
 else:
-    with open('counts.json', 'r') as f:
+    with open('map.json', 'r') as f:
         counts = json.loads(f.read())
+    with open('last_watched.txt', 'r') as f:
+        last_watched = int(f.read())
 
 
 '''Find out what next to watch'''
+if last_watched == 233:
+    last_watched = 0
+else:
+    last_watched += 1
 
-# Average number of times each episode has been watched
-avg = sum(counts[i] for i in counts.keys()) / len(counts)
+try:
+    new = counts[last_watched]
+except KeyError:
+    new = counts[str(last_watched)]
 
-# New episode to watch
-new = random.choice(list(counts.keys()))
-
-# Don't watch an overwatched episode
-while counts[new] > avg:
-    new = random.choice(list(counts.keys()))
-
-# Add the count of the new episode
-counts[new] += 1
-
-with open('counts.json', 'w') as f:
-    f.write(json.dumps(counts))
+with open('last_watched.txt', 'w') as f:
+    f.write(str(last_watched))
 
 '''Play the episode'''
 if sys.platform == 'darwin':
@@ -73,7 +78,7 @@ elif sys.platform in ['win32', 'win64']:
     #    "If you're nerd enough to use this, why still on windows?")
     # If you're still stuck in your parents basement and don't know it,
     # some people are stuck with windows... and hate it
-    os.system('cmd /c ' +'"' +new+'"')
+    os.system('cmd /c ' + '"' + new + '"')
 else:
     raise Exception("Platform not recognized. Maybe too much nerd for this?")
 
