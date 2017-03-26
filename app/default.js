191952ea6b60c300af199cf6399732b2ac7340c4
Himanshu Mishra <himanshu2014iit@gmail.com>
2017-03-26 14:59:42 +0530

    Initialize electron app. Add dynamic list

---
+++ app/default.js
@@ -0,0 +1,49 @@
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    if (confirm("Are you sure to remove? \n\nAll the settings and database will be lost.")) {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+function add_button(argument) {
+  var li = document.createElement("li");
+  // var inputValue = document.getElementById("myInput").value;
+  var inputValue = "Hi";
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("list").appendChild(li);
+  }
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
